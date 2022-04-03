ef09652644c5c94ad31eda9e7262f6d87d89d7fa
Mateusz-Malendowski <81644484+Mateusz-Malendowski@users.noreply.github.com>
2022-04-03 14:13:34 +0200

    Update szkielet.py

--- backend/szkielet.py
+++ backend/szkielet.py
@@ -1,31 +1,42 @@
 import socket
+import mysql.connector
 
 def launch_params():
-    host = ''     
+    host = ''
     port = 12345
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((host, port))
-    s.listen(1)
-    con, addr = s.accept()
-    return s, con, addr
+    s.listen(5)
+
+    db = mysql.connector.connect(host="localhost",
+                            user="wspauser",
+                            password="password",
+                            db="wspa")
+    cur = db.cursor()
+    return s, db, cur
 
 if __name__ == "__main__":
     
-    s, con, addr = launch_params()
-    
-    print('Connected', addr)
+    s, db, cur = launch_params()
     
     while True:
         try:
-            data = con.recv(1024)
-
-            if not data: break
+            con, addr = s.accept()
+            print('Connected', addr)
+            data = con.recv(1024)               #server awaits data    
 
             print(b"Data: " + data)
-            con.sendall(b"Server reply")
+            con.sendall(b"Server reply")        #serwer sends data
+
+            cur.execute("SELECT * FROM test")   #SELECT query
+
+            for x in cur.fetchall():            #prints first cell of all rows
+                print(row[0])
 
         except socket.error:
             print("Error.")
             break
 
     con.close()
+    s.close()
+    db.close()
