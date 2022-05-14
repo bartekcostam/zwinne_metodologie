5f9ee0dab57116bb8fc2aa49148e868e6c0ea4a1
Karol Sitarz <ka.sitarz@gmail.com>
2022-05-14 13:03:02 +0200

    add input component add button component add register form

---
+++ frontend/pages/auth/register.tsx
@@ -0,0 +1,24 @@
+import { Input } from 'components/Input'
+import { HiAtSymbol } from 'react-icons/hi'
+import { RiLockPasswordLine } from 'react-icons/ri'
+import { Button } from 'components/Button'
+
+export default function Register() {
+  // const test = useQuery('categories', '/api/test')
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <form className="p-6 max-w-sm w-full m-6 rounded-2xl bg-white shadow-2xl">
+        <Input placeholder="Email" type="email" icon={HiAtSymbol} />
+        <Input placeholder="Hasło" type="password" icon={RiLockPasswordLine} />
+        <Input
+          placeholder="Potwierdź hasło"
+          type="password"
+          icon={RiLockPasswordLine}
+        />
+        <Button type="submit" block>
+          Rejestracja
+        </Button>
+      </form>
+    </div>
+  )
+}
